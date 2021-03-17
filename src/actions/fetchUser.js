4d48a04e7adf6f3cf2375f49fe1bf7f3ed9ec26a
snowing222 <snowing222@gmail.com>
2021-03-17 16:39:00 -0400

    setup heroku env token

--- src/actions/fetchUser.js
+++ src/actions/fetchUser.js
@@ -1,4 +1,87 @@
 
+// export function createUser(userObj, history) {
+//   return (dispatch) => {
+//     let configObj = {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//         Accept: "application/json",
+//       },
+//       body: JSON.stringify({ user: userObj }),
+//     };
+
+//     return fetch("https://homeaway-backend.herokuapp.com/api/v1/users", configObj)
+//       .then((resp) => resp.json())
+//       .then((data) => {
+//         if (data.errors) {
+//           alert(data.errors);
+//         } else {
+//           localStorage.setItem("token", data.jwt);
+//           dispatch({ type: "LOG_IN_USER", payload: data.user });
+//           history.push("/myprofile");
+//         }
+//       });
+//   };
+// }
+
+// export function loginUser(userObj, history) {
+//   return (dispatch) => {
+//     let configObj = {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//         Accept: "application/json",
+//       },
+//       body: JSON.stringify({ user: userObj }),
+//     };
+
+//     return fetch("https://homeaway-backend.herokuapp.com/api/v1/login", configObj)
+//       .then((resp) => {console.log(resp)
+//       return resp.json()} )
+//       .then((data) => {
+//         if (data.errors) {
+//           alert(data.errors);
+//         } else {
+        
+//           localStorage.setItem("token", data.jwt);
+//           dispatch({ type: "LOG_IN_USER", payload: data.user });
+//           history.push("/myprofile");
+//         }
+//       });
+//   };
+// }
+
+// export function fetchLoggedInUser() {
+//   return (dispatch) => {
+//     const token = localStorage.token;
+//     console.log(token)
+//     let configObj = {
+//       method: "GET",
+//       headers: {
+//         "Content-Type": "application/json",
+//         Accept: "application/json",
+//         Authorization: `Bearer ${token}`
+//       }
+//     }
+
+
+//     if (token) {
+//       return fetch("https://homeaway-backend.herokuapp.com/api/v1/auto_login", configObj)
+//       .then((resp) => {console.log(resp)
+//         return resp.json()})
+//       .then((data) => {
+//           console.log(data);
+//           if (data.errors) {
+//             alert(data.errors);
+//             localStorage.removeItem("token");
+//           } else {
+//             dispatch({ type: "LOG_IN_USER", payload: data.user });
+//           }
+//         });
+//     }
+//   };
+// }
+
 export function createUser(userObj, history) {
   return (dispatch) => {
     let configObj = {
@@ -10,7 +93,7 @@ export function createUser(userObj, history) {
       body: JSON.stringify({ user: userObj }),
     };
 
-    return fetch("https://homeaway-backend.herokuapp.com/api/v1/users", configObj)
+    return fetch("http://localhost:3001/api/v1/users", configObj)
       .then((resp) => resp.json())
       .then((data) => {
         if (data.errors) {
@@ -35,7 +118,7 @@ export function loginUser(userObj, history) {
       body: JSON.stringify({ user: userObj }),
     };
 
-    return fetch("https://homeaway-backend.herokuapp.com/api/v1/login", configObj)
+    return fetch("http://localhost:3001/api/v1/login", configObj)
       .then((resp) => {console.log(resp)
       return resp.json()} )
       .then((data) => {
@@ -66,7 +149,7 @@ export function fetchLoggedInUser() {
 
 
     if (token) {
-      return fetch("https://homeaway-backend.herokuapp.com/api/v1/auto_login", configObj)
+      return fetch("http://localhost:3001/api/v1/auto_login", configObj)
       .then((resp) => {console.log(resp)
         return resp.json()})
       .then((data) => {
@@ -81,3 +164,4 @@ export function fetchLoggedInUser() {
     }
   };
 }
+
