c0e93b046ad7027454d045b5ea4498c7f1a8d978
snowing222 <snowing222@gmail.com>
2021-03-18 22:36:41 -0400

    edit component

--- src/containers/MyProfileContainer.js
+++ src/containers/MyProfileContainer.js
@@ -15,67 +15,13 @@ class MyProfileContainer extends Component {
 
   render() {
     return (
-      // <div>
+      <div>
      
-      //     <div id="sidebar-wrapper"> 
-      //       <Sidebar />
-      //     </div>
-
-      //     <div id="page-content-wrapper">
-      //     <Switch>
-      //         <Route
-      //           exact
-      //           path="/myprofile/properties/new"
-      //           component={PropertyInput}
-      //         />
-
-      //         <Route
-      //           exact
-      //           path="/myprofile/properties"
-      //           render={(routerProps) => (
-      //             <PropertiesContainer
-      //               {...routerProps}
-      //               user={this.props.user.user}
-      //             />
-      //           )}
-      //         />
-      //         <Route
-      //           exact
-      //           path="/myprofile/listings"
-      //           component = {UserListingsContainer}
-      //         />
-
-      //         <Route
-      //           path="/myprofile/properties/:propertyId/listings/new"
-      //           component={ListingInput}
-      //         />
-      //         <Route
-      //           path="/myprofile/properties/:propertyId/listings/:listingId"
-      //           render={(routerProps) => <Listing {...routerProps} />}
-      //         />
-      //         <Route
-      //           path="/myprofile/properties/:propertyId"
-      //           render={(routerProps) => (
-      //             <PropertyShowPage
-      //               {...routerProps}
-      //               user={this.props.user}
-      //               properties={this.props.user.user.properties}
-      //             />
-      //           )}
-      //         />
-      //       </Switch>
-      //     </div>
-      // </div>
- 
-
-
- <Container fluid>
-        <Row>
-          <Col lg={2} id="sidebar-wrapper"> 
+          <div id="sidebar-wrapper"> 
             <Sidebar />
-          </Col>
+          </div>
 
-          <Col lg={10} id="page-content-wrapper">
+          <div id="page-content-wrapper">
           <Switch>
               <Route
                 exact
@@ -118,9 +64,63 @@ class MyProfileContainer extends Component {
                 )}
               />
             </Switch>
-          </Col>
-        </Row>
-      </Container>
+          </div>
+      </div>
+ 
+
+
+//  <Container fluid>
+//         <Row>
+//           <Col lg={2} id="sidebar-wrapper"> 
+//             <Sidebar />
+//           </Col>
+
+//           <Col lg={10} id="page-content-wrapper">
+//           <Switch>
+//               <Route
+//                 exact
+//                 path="/myprofile/properties/new"
+//                 component={PropertyInput}
+//               />
+
+//               <Route
+//                 exact
+//                 path="/myprofile/properties"
+//                 render={(routerProps) => (
+//                   <PropertiesContainer
+//                     {...routerProps}
+//                     user={this.props.user.user}
+//                   />
+//                 )}
+//               />
+//               <Route
+//                 exact
+//                 path="/myprofile/listings"
+//                 component = {UserListingsContainer}
+//               />
+
+//               <Route
+//                 path="/myprofile/properties/:propertyId/listings/new"
+//                 component={ListingInput}
+//               />
+//               <Route
+//                 path="/myprofile/properties/:propertyId/listings/:listingId"
+//                 render={(routerProps) => <Listing {...routerProps} />}
+//               />
+//               <Route
+//                 path="/myprofile/properties/:propertyId"
+//                 render={(routerProps) => (
+//                   <PropertyShowPage
+//                     {...routerProps}
+//                     user={this.props.user}
+//                     properties={this.props.user.user.properties}
+//                   />
+//                 )}
+//               />
+//             </Switch>
+//           </Col>
+//         </Row>
+//       </Container>
        
     );
   }
